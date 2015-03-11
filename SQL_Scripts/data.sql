3bff52bf092b36cc77b5fc7137d09c54d054fe6b
lolicatgirls <devito@ualberta.ca>
2015-03-10 21:04:08 -0600

    Started on populating tables

---
+++ SQL_Scripts/data.sql
@@ -0,0 +1,48 @@
+INSERT INTO persons
+VALUES
+(2,'James','Devito', '1234 aroad, Edmonton, AB, A1B-2C3, CA',
+'devito@ualberta.ca', '159-5633');
+
+INSERT INTO persons
+VALUES
+(3,'Cheng','Chen', '8888 Another road, Edmonton, A2D-3D3, CA',
+'chengsemail@email.com', '123-9999');
+
+INSERT INTO persons
+VALUES
+(4,'Commandeur', 'Brett', '1111 something, Edmonton, V3G-9D2, CA',
+'brettsemail@email.com', '324-4214');
+INSERT INTO persons
+VALUES
+(5,'Thomas', 'Rigde', '4345 Barnroad, Fort McMurry, BC, F5G-9S1, CA',
+'TOM@hotmail.com', '424-5334');
+
+INSERT INTO persons
+VALUES
+(6, 'Forester', 'Bob', '7656 Forestroad, ForestTown, BC, F2A-1G7, CA',
+'bob@forest.com', '424-2222');
+
+INSERT INTO persons
+Values
+(7, 'Dan', 'DannyBoy', '3145 DannyRoad, DanCity, AB, E1C-5R2, CA',
+'DAN@gmail.com', '555-5555');
+
+INSERT INTO persons
+values
+(8, 'Stan', 'Man', '5435 theroad, WhiteCourt, AB, D5Z-3P1, CA',
+'stan@gmail.com', '634-1235');
+
+insert into persons
+values
+(9, 'ANAME' , 'ANOTHERNAME', '1222 froad, thecity, AB, E1F-5E2, CA',
+'aname@gmail.com', '423-5521');
+
+insert into persons
+values
+(10, 'Pedro','Mang', '6345 mangroad, CoolTown, YT, Y7Z-2E2, CA',
+'pedro@gmail.com', '421-9343');
+
+insert into persons
+values
+(11, 'Alan', 'Major', '8567 Major Street, majorTown, YT, T4Y-0S2, CA',
+'alan@gmail.com', '634-2432');
